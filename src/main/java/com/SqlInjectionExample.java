c3b8f781f57c0dd173f6fbfa84e44522b960a6cb
lxz <250390627@qq.com>
2025-10-17 15:00:28 +0800

    sql

--- src/main/java/com/SqlInjectionExample.java
+++ src/main/java/com/SqlInjectionExample.java
@@ -9,40 +9,40 @@ import java.sql.Statement;
 
 public class SqlInjectionExample {
 
-    public void doLogin(String user, String pass) {
-        Connection conn = null;
-        Statement stmt = null;
-        try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=myuser&password=mypass");
+    // public void doLogin(String user, String pass) {
+    //     Connection conn = null;
+    //     Statement stmt = null;
+    //     try {
+    //         conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=myuser&password=mypass");
 
-            // Unsafe SQL query constructed by concatenating strings.
-            String query = "SELECT * FROM users WHERE user = '" + user + "' AND password = '" + pass + "'";
+    //         // Unsafe SQL query constructed by concatenating strings.
+    //         String query = "SELECT * FROM users WHERE user = '" + user + "' AND password = '" + pass + "'";
 
-            stmt = conn.createStatement();
-            stmt.executeQuery(query);
+    //         stmt = conn.createStatement();
+    //         stmt.executeQuery(query);
 
-            // ...
+    //         // ...
 
-        } catch (SQLException se) {
-            // Handle errors for JDBC
-            se.printStackTrace();
-        } finally {
-            // Finally block used to close resources
-            try {
-                if (stmt != null) {
-                    stmt.close();
-                }
-            } catch (SQLException se2) {
-            }
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }
-        }
-    }
+    //     } catch (SQLException se) {
+    //         // Handle errors for JDBC
+    //         se.printStackTrace();
+    //     } finally {
+    //         // Finally block used to close resources
+    //         try {
+    //             if (stmt != null) {
+    //                 stmt.close();
+    //             }
+    //         } catch (SQLException se2) {
+    //         }
+    //         try {
+    //             if (conn != null) {
+    //                 conn.close();
+    //             }
+    //         } catch (SQLException se) {
+    //             se.printStackTrace();
+    //         }
+    //     }
+    // }
     
    public void doLogin(String user, String pass) {
        Connection conn = null;
